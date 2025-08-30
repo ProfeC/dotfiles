552085bdd747145c81f81eb31203d02a33619e50
ProfeC <gclark2@gmail.com>
2025-08-30 13:24:50 -0400

    adds module for vivaldi browser.

--- profiles/mac-mini-16g.nix
+++ profiles/mac-mini-16g.nix
@@ -8,6 +8,7 @@
     ../modules/system/bluetooth.nix
     # ../modules/browsers/brave.nix
     ../modules/browsers/firefox.nix
+    ../modules/browsers/vivaldi.nix
     ../modules/desktops/kde-plasma.nix
     # ../modules/virtualization/virtualization.nix
     ../modules/system/x11.nix
