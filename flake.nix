32dd0dfbab6c5e04325949f5ef8774f51859ce19
G. Lee Clark, II <gclark2@gmail.com>
2025-09-04 23:25:39 -0400

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -34,90 +34,81 @@
     # };
   };
 
-  outputs = { 
-    flake-utils,
-    home-manager,
-    nixos-hardware, 
-    nixos-wsl, 
-    nixpkgs,
-    nur,
-    self, 
-    # stylix,
-    systems,
-    ... 
-  }:
-  let
-    mySystem = "x86_64-linux";
-    pkgsFor = system: import nixpkgs { inherit system; };
-  in {
-    nixosConfigurations = {
-      # Clone repo with `git clone https://github.com/you/dotfiles.git /etc/nixos`
+  outputs = inputs:
+    let
+      mySystem = "x86_64-linux";
+      flakeContext = {
+        inherit inputs;
+      };
+    in {
+      nixosConfigurations = {
+        # Clone repo with `git clone https://github.com/you/dotfiles.git /etc/nixos`
 
-      # Mac Mini (2014) 16GB Ram 1 TB HD
-      # Switch with `sudo nixos-rebuild switch --flake /etc/nixos#mac-mini-01`
-      mac-mini-01 = nixpkgs.lib.nixosSystem {
-        system = mySystem;
-        modules = [
-          ./profiles/mac-mini-16g.nix
-          ./modules/users/lee.nix
-          ./modules/users/serveradmin.nix
-          # stylix.nixosModules.stylix
-        ];
+        # Mac Mini (2014) 16GB Ram 1 TB HD
+        # Switch with `sudo nixos-rebuild switch --flake /etc/nixos#mac-mini-01`
+        mac-mini-01 = nixpkgs.lib.nixosSystem {
+          system = mySystem;
+          modules = [
+            ./profiles/mac-mini-16g.nix
+            ./modules/users/lee.nix
+            ./modules/users/serveradmin.nix
+            # stylix.nixosModules.stylix
+          ];
+        };
+
+        # Portable USB Drive
+        # Switch with `sudo nixos-rebuild switch --flake /etc/nixos#usb-drive`
+        usb-drive = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./profiles/usb-drive-samsung-64g.nix
+            ./modules/users/lee.nix
+          ];
+        };
+
+        # SHU Laptop - Lenovo ThinkPad T14s
+        # Switch with `sudo nixos-rebuild switch --flake /etc/nixos#shu-lappy`
+        shu-lappy = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./profiles/shu-t14s.nix
+            ./modules/users/lee.nix
+            ./modules/users/shu-clarkgar.nix
+          ];
+        };
+
+        # Windows WSL2
+        # Switch with `sudo nixos-rebuild switch --flake /etc/nixos#wsl2`
+        wsl2 = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            nixos-wsl.nixosModules.default
+            ./profiles/wsl2.nix
+            # ./modules/users/lee.nix
+            # ./modules/users/shu-clarkgar.nix
+          ];
+        };
+
+        # Generic (Default) Minimal Config
+        # Switch with `sudo nixos-rebuild switch --flake /etc/nixos#generic`
+        generic = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            ./modules/common.nix
+            ./modules/users/lee.nix
+            ./modules/users/serveradmin.nix
+            ./hosts/default/configuration.nix
+            ./modules/desktops/kde-plasma.nix
+            ./modules/system/audio-pipewire.nix
+          ];
+        };
       };
 
-      # Portable USB Drive
-      # Switch with `sudo nixos-rebuild switch --flake /etc/nixos#usb-drive`
-      usb-drive = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./profiles/usb-drive-samsung-64g.nix
-          ./modules/users/lee.nix
-        ];
-      };
-
-      # SHU Laptop - Lenovo ThinkPad T14s
-      # Switch with `sudo nixos-rebuild switch --flake /etc/nixos#shu-lappy`
-      shu-lappy = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./profiles/shu-t14s.nix
-          ./modules/users/lee.nix
-          ./modules/users/shu-clarkgar.nix
-        ];
-      };
-
-      # Windows WSL2
-      # Switch with `sudo nixos-rebuild switch --flake /etc/nixos#wsl2`
-      wsl2 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          nixos-wsl.nixosModules.default
-          ./profiles/wsl2.nix
-          # ./modules/users/lee.nix
-          # ./modules/users/shu-clarkgar.nix
-        ];
-      };
-
-      # Generic (Default) Minimal Config
-      # Switch with `sudo nixos-rebuild switch --flake /etc/nixos#generic`
-      generic = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./modules/common.nix
-          ./modules/users/lee.nix
-          ./modules/users/serveradmin.nix
-          ./hosts/default/configuration.nix
-          ./modules/desktops/kde-plasma.nix
-          ./modules/system/audio-pipewire.nix
-        ];
-      };
-    };
-
-    # 👇 Add devShells
-    devShells = {
-      mySystem = {
-        shu-docs = import ./devShells/shu-docs.nix flakeContext { system = "x86_64-linux"; };
+      # 👇 Add devShells
+      devShells = {
+        mySystem = {
+          shu-docs = import ./devShells/shu-docs.nix flakeContext { system = "x86_64-linux"; };
+        };
       };
     };
-  };
 }
