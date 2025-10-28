1af4cee044c1c4e323b824cd8a73f873e395bcb6
ProfeC <gclark2@gmail.com>
2025-10-28 15:48:41 -0400

    cleans and formats wsl2.nix

--- profiles/wsl2.nix
+++ profiles/wsl2.nix
@@ -1,20 +1,15 @@
 # profiles/wsl2.nix
-
 # Edit this configuration file to define what should be installed on
 # your system. Help is available in the configuration.nix(5) man page, on
 # https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 # NixOS-WSL specific options are documented on the NixOS-WSL repository:
 # https://github.com/nix-community/NixOS-WSL
-
 {
-  config, 
-  pkgs, 
-  lib, 
-  ... 
-}:
-
-{
+  config,
+  pkgs,
+  lib,
+  ...
+}: {
   imports = [
     #   # include NixOS-WSL modules
     #   <nixos-wsl/modules>
@@ -38,7 +33,7 @@
     shellAliases = {
       ll = "ls -lah";
       lt = "ls -laht";
-    }
+    };
   };
 
   wsl = {
@@ -53,9 +48,7 @@
 
   users.users.nixos = {
     shell = pkgs.zsh;
-    openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCtmsr35Syy1YILOvsOnsXqUG7gdzz5FWmpiC0TNhD0dbehsamuetfUjlmOTaJzz1US93aBFsVDmE89R7XeWMqUjRzvDYE1RrW6QvDN+pHKOqyzqvoPuJmKE4L9HRpcLKj6Jn8SoFhzRBwu96tgg/99XWEQ8MSgyoGD728CIe9iWqvfJB0Z6wNK72YSGe1sF6JleUNvjmmEumSgIwIcAQ4kzFEowt8m41jhWhhEHz4Yv6LYzHILZmMqpAswzPXdNhkXp4wPnh5fw8BMRZtJnwp2MkpKLik5N6uNi/EtyRU/mW0H/9jyYr0rVeSiJt3SQgiDiuR/3iMI7pmMqmLjcCWzaQ12qZR2YhZrMOfvydpLZMGPZH2zm4CdColHw03Kg/NWS+IoJC94NNlyM5wkDJCVV2TdLcC+xeXEopto5f9y6SiRg5QueiJ01oIAvjCcl42mZ2wu1suviwjDCT3nbZG8URRdWJQ3FszOFyUP2TMEzNDtpsU+oe7zG9Y9tmBdNDk= clarkgar@shu-t480s-clarkgar"
-    ];
+    # openssh.authorizedKeys.keys = [];
   };
 
   # This value determines the NixOS release from which the default
