628e854d969c8bc2ea19ebef928c15543ee6c109
ProfeC <gclark2@gmail.com>
2025-10-28 15:50:54 -0400

    cleans and formats wezterm.nix

--- modules/home/lee/wezterm.nix
+++ modules/home/lee/wezterm.nix
@@ -4,22 +4,12 @@
   lib,
   pkgs,
   ...
-}:
-
-let
+}: let
   wezterm_extra = builtins.readFile ./config/wezterm/wezterm-extra.lua;
 in {
   programs.wezterm = {
     enable = true;
     package = pkgs.wezterm;
-    # Option A: inline config (small)
-    extraConfig = ''
-      -- inline Lua; small example
-      -- We keep it short here; you can drop the full wezterm.lua content into a file and reference it.
-      local wezterm = require("wezterm")
-      wezterm.on("gui-startup", function(cmd)
-        -- nothing fancy here; the full desktop config is in ./config/wezterm.
-      end)
-    '';
+    extraConfig = wezterm_extra;
   };
 }
