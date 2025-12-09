f9a2db7466e59d8086aa1f5e92c87a05ad24f25b
ProfeC <gclark2@gmail.com>
2025-12-08 21:33:28 -0500

    Update steam.nix

--- modules/gaming/steam.nix
+++ modules/gaming/steam.nix
@@ -6,12 +6,15 @@
 }: {
   # Enable the Steam service
   environment.systemPackages = with pkgs; [
+    cmake # Cross-platform, open-source build system generator
     steam
+    steam-rom-manager # App for adding 3rd party games/ROMS as Steam launch items.
   ];
 
   # Other configurations to optimize Steam
   # services.xserver.videoDrivers = [ "nvidia" ];  # or "intel", "amd", etc.
   hardware.graphics.enable = true;
+  hardware.xone.enable = true;
 
   # Configure settings for running Steam
   users.users.lee = {
